803c0840874f928719b6de440e5ed978171e9586
Elvin O. Ortiz Collazo <elvin.ortiz3@upr.edu>
2018-10-14 18:15:52 -0400

    second upload

--- Main.py
+++ Main.py
@@ -1,8 +1,8 @@
-import os
+[5:24 PM, 10/14/2018] +1 (787) 391-8188: import os
 import pygame
-from Tkinter.filedialog import askdirectory
+from tkinter.filedialog import askdirectory
 
-from Tkinter import *
+from tkinter import *
 
 root = Tk()
 root.minsize(300,300)
@@ -32,5 +32,54 @@ listbox.pack()
 
 directorychooser()
 
+
+root.mainloop()
+[6:10 PM, 10/14/2018] +1 (787) 391-8188: javihdznovoa
+[6:13 PM, 10/14/2018] +1 (787) 391-8188: import os
+import pygame
+from tkinter.filedialog import askdirectory
+
+from tkinter import *
+
+root = Tk()
+root.minsize(300,300)
+
+listofsongs = []
+
+index = 0
+
+
+def directorychooser():
+
+    directory = askdirectory()
+    os.chdir(directory)
+
+    for files in os.listdir(directory):
+        if files.endswith(".mp3"):
+            listofsongs.append(files)
+            print(files)
+    pygame.mixer.init()
+    pygame.mixer.music.load(listofsongs[0])
+    pygame.mixer.music.play()
+
+label = Label(root,text="Music Player")
+label.pack()
+
+listbox = Listbox(root)
+listbox.pack()
+
+for items in listofsongs:
+    listbox.insert(END,items)
+
+nextbutton = Button(root, text = "Next Song")
+nextbutton.pack()
+
+previousbutton = Button(root, text = "Previous Song")
+previousbutton.pack()
+
+stopbutton = Button(root, text = "Stop Song")
+stopbutton.pack()
+
+directorychooser()
 
 root.mainloop()
