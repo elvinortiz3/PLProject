8536dc7c04ebe4e14e39b6c47876c9903fe8132b
AlexVR <josian.velez@upr.edu>
2018-10-29 14:03:12 -0400

    BaseFile Created

--- PySharp_executable/PySharp_Converter.py
+++ PySharp_executable/PySharp_Converter.py
@@ -4,13 +4,6 @@ import re
 # Function to exchange all operators with those evaluated by python.
 def replace_operators(expr):
     REPLACEMENTS = {
-        'NOT': ' not ',
-        'OR': ' or ',
-        'AND': ' and ',
-        'XOR': ' ^ ',
-        'NAND': ' and ',
-        'NOR': ' or ',
-        'XNOR': ' ^ ',
     }
     return re.sub('|'.join(re.escape(sym) for sym in REPLACEMENTS.keys()),
                   lambda sym: REPLACEMENTS[sym.group(0)],expr).strip()
